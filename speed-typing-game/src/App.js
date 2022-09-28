6968a3e52199a99d9360d614696ae2070bdf4ee2
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2022-09-28 15:59:55 +0530

    Completed the basic structure of the web page

---
+++ speed-typing-game/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>How fast do you type?</h1>
+      <textarea rows="10" cols="54"/>
+      <h4>Time remaining: 10</h4>
+      <button>Start</button>
+      <h1>Word Count: 1</h1>
+    </div>
+  );
+}
+
+export default App;
